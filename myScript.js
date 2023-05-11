8a7cb87edf8007716b7563a87aa0924c71113071
XDancer <111144908+andylebeau@users.noreply.github.com>
2023-05-11 14:38:59 -0400

    playerSelection to first letter cap

--- myScript.js
+++ myScript.js
@@ -1,1 +1,6 @@
 const getComputerChoice = () => ['Rock', 'Paper', 'Scissors'][Math.floor(Math.random() * 3)];
+
+function playRound (playerSelection, computerSelection) {
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    console.log(playerSelection)
+}
