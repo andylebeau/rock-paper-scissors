0306fc354f69c6d84db35f929defe48cd772d43d
XDancer <111144908+andylebeau@users.noreply.github.com>
2023-05-11 15:19:03 -0400

    function playRound attemp

--- myScript.js
+++ myScript.js
@@ -1,6 +1,22 @@
 const getComputerChoice = () => ['Rock', 'Paper', 'Scissors'][Math.floor(Math.random() * 3)];
 
+const correctPlayerFormat = str => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+
 function playRound (playerSelection, computerSelection) {
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-    console.log(playerSelection)
+    playerSelection = correctPlayerFormat(playerSelection)
+    if (playerSelection == computerSelection) {
+        return "It's a draw!"
+    }
+    else if (playerSelection == 'Rock') {
+        return (computerSelection == 'Paper') ?
+        `You Lose! ${computerSelection} beats ${playerSelection}.` : `You Win! ${playerSelection} beats ${computerSelection}.`
+    }
+    else if (playerSelection == 'Paper') {
+        return (computerSelection == 'Scissors') ?
+        `You Lose! ${computerSelection} beats ${playerSelection}.` : `You Win! ${playerSelection} beats ${computerSelection}.`
+    }
+    else if (playerSelection == 'Scissors') {
+        return (computerSelection == 'Rock') ?
+        `You Lose! ${computerSelection} beats ${playerSelection}.` : `You Win! ${playerSelection} beats ${computerSelection}.`
+    }
 }
