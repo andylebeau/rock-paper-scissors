fa2939c4386b89d47def38ffe7f2d424fa812154
XDancer <111144908+andylebeau@users.noreply.github.com>
2023-05-12 16:13:15 -0400

    function 'game' added to loop 5 times

--- myScript.js
+++ myScript.js
@@ -1,36 +1,46 @@
 const getComputerChoice = () => ['Rock', 'Paper', 'Scissors'][Math.floor(Math.random() * 3)];
-const getPlayerChoice = () => ['Rock', 'Paper', 'Scissors'][Math.floor(Math.random() * 3)];
-
 const correctPlayerFormat = str => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 
 function playRound (playerSelection, computerSelection) {
-    // playerSelection = correctPlayerFormat(playerSelection);
-    playerSelection = getPlayerChoice();
+    playerSelection = correctPlayerFormat(playerSelection)
     computerSelection = getComputerChoice();
     if (playerSelection == computerSelection) {
         return "It's a draw!"
     }
     else if (playerSelection == 'Rock') {
-        return (computerSelection == 'Paper') ?
-        `You Lose! ${computerSelection} beats ${playerSelection}.` : `You Win! ${playerSelection} beats ${computerSelection}.`
+        return (computerSelection == 'Scissors') ?
+        `You Win! ${playerSelection} beats ${computerSelection}.` : `You Lose! ${computerSelection} beats ${playerSelection}.`
     }
     else if (playerSelection == 'Paper') {
-        return (computerSelection == 'Scissors') ?
-        `You Lose! ${computerSelection} beats ${playerSelection}.` : `You Win! ${playerSelection} beats ${computerSelection}.`
+        return (computerSelection == 'Rock') ?
+        `You Win! ${playerSelection} beats ${computerSelection}.` : `You Lose! ${computerSelection} beats ${playerSelection}.`
     }
     else if (playerSelection == 'Scissors') {
-        return (computerSelection == 'Rock') ?
-        `You Lose! ${computerSelection} beats ${playerSelection}.` : `You Win! ${playerSelection} beats ${computerSelection}.`
+        return (computerSelection == 'Paper') ?
+        `You Win! ${playerSelection} beats ${computerSelection}.` : `You Lose! ${computerSelection} beats ${playerSelection}.`
     }
 }
 
-function game () {
-    let countPlayerWins = 0;
+function game() {
+    let playerTotalWins = 0
+    let computerTotalWins = 0
+    let draws = 0
+
     for (let i = 0; i < 5; i++) {
-        console.log(playRound(getPlayerChoice(), getComputerChoice))
-        if (playRound(getPlayerChoice(), getComputerChoice()).includes('Win')) {
-            countPlayerWins++;
+        const playerSelection = prompt('Please type rock, paper or scissors')
+        const computerSelection = getComputerChoice;
+        let winOrLose = playRound(playerSelection, computerSelection)
+        if (winOrLose.includes('Win')) {
+            playerTotalWins++;
         }
+        else if (winOrLose.includes('Lose')) {
+            computerTotalWins++
+        }
+        else {
+            draws++
+        }
+        console.log(winOrLose)
     }
-    return countPlayerWins > 2 ? `You won ${countPlayerWins} out of 5 rounds!` : `You lost ${countPlayerWins} out of 5 rounds}.`
-}
+    return (draws == 3 ? "It's a draw!" :
+        playerTotalWins > (5 - draws) / 2) ? `You Won! Out of 5 Rounds, you won ${playerTotalWins} with ${draws} draws. The computer won ${computerTotalWins} rounds` :
+        `You Lost! Out of 5 Rounds, the computer won ${5 - playerTotalWins - draws} rounds with ${draws} draws.`}
